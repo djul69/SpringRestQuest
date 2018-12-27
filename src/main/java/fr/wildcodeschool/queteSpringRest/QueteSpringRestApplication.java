cb9deea7b6bc0fa38abb7f03cb9c2b68e14baf2e
djul69 <julien.cordenod@pole-emploi.fr>
2018-12-27 15:10:22 +0100

    first commit

---
+++ src/main/java/fr/wildcodeschool/queteSpringRest/QueteSpringRestApplication.java
@@ -0,0 +1,14 @@
+package fr.wildcodeschool.queteSpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteSpringRestApplication.class, args);
+	}
+
+}
+
